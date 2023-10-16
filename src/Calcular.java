7be592bea2c89c53198cbd6481715ef80b7aa928
Gb-dev371 <gbdev371@gmail.com>
2023-10-16 11:38:08 -0300

    Código feito com os métodos de cálculo na classe Calcular.

---
+++ src/Calcular.java
@@ -0,0 +1,17 @@
+public class Calcular {
+    public static double Soma(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double Subtracao(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double Multiplicacao(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double Divisao(double num1, double num2){
+        return num1 / num2;
+    }
+}
